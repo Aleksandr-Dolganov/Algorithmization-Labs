30af2420fcd2085703541fc7ecb634037b92e0c1
Sam Wilde <DolganovAS@mer.ci.nsu.ru>
2020-11-30 18:42:24 +0700

    Zadania 1-2 za 30.11.20

---
+++ 30.11.20/30.11.20-2/30.11.20-2.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+/*2.  Дан файл input.txt содержащий целые числа. Записать в файл  output.txt
+компоненты исходного файла, являющиеся простыми числами.*/
+
+int main()
+{
+	ifstream file("E:\\file\\2\\input.txt");
+	ofstream output("E:\\file\\2\\output.txt");
+	int a[99], j = 0;
+	bool flag = false;
+	for(int i = 0; i < 99 and !file.eof(); i++)
+	{
+		file >> a[i];
+		j++;
+	}
+	for(int k = 0, n = 0; k < j; k++)
+	{
+		for(int i = 1; i * i <= a[k]; i++)
+		{
+			if(a[k] % i == 0)
+			{
+				n++;
+			}
+			if(n > 1)
+			{
+				flag = false;
+				break;
+			}
+			else
+			{
+				flag = true;
+			}
+		}
+		if(flag and a[k] != 1)
+		{
+			output << a[k] << " ";
+			cout << a[k] << " ";
+		}
+		flag = true;
+		n = 0;
+	}
+
+	file.close();
+	output.close();
+}
---
+++ 30.11.20/30.11.20/30.11.20.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+/*1.  Имеется текстовый файл input.txt Сформировать новый файл, содержащий слова,
+сформированные из первых букв слов каждой строки исходного файла.
+Результат работы программы (вывод) поместить в отдельный текстовый файл
+(например, output.txt ), продублировав на экране.*/
+
+int main()
+{
+	ifstream file;
+	file.open("E:\\file\\1\\input.txt");
+	string s[99];
+	int i = 0, j = 0;
+	while(!file.eof())
+	{
+		file >> s[i];
+		j++;
+		i++;
+	}
+	for(int i = 0; i < j; i++)
+	{
+		cout << s[i][0, 0];
+	}
+	ofstream output;
+	output.open("E:\\file\\1\\output.txt");
+	for(int i = 0; i < j; i++)
+	{
+		output << s[i][0, 0];
+	}
+	file.close();
+	output.close();
+}
