8ce1040dee5f11efe82b10be57a2e6b7bf477ae8
Sam Wilde <DolganovAS@mer.ci.nsu.ru>
2020-12-07 15:05:10 +0700

    1-4 zadania za 07.12.20

---
+++ 07.12.20/07.12.20/1 zadanie.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+/* 1. Дана строка. Подсчитать, сколько в ней букв y, x, z */
+
+int main()
+{
+	char a[40];
+	gets_s(a);
+	int county = 0, countx = 0, countz = 0, size = strlen(a);
+	for(int i = 0; i < size; i++)
+	{
+		if(a[i] == 'y')
+		{
+			county++;
+		}
+		if(a[i] == 'z')
+		{
+			countz++;
+		}
+		if(a[i] == 'x')
+		{
+			countx++;
+		}
+	}
+	std::cout << "x=" << countx << "\ny=" << county << "\nz=" << countz;
+	return 0;
+}
